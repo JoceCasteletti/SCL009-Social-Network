7ae56124ddff4187b7b06799aca38ddb5fe24bca
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-06-04 02:07:12 -0400

    se cambia evento del dom en route ,se crea carpeta test y se modifica id de elemento del dom

--- src/assets/js/authdom.js
+++ src/assets/js/authdom.js
@@ -6,7 +6,7 @@ export const createNewUser = () => {
     let name= document.getElementById('name').value;
     let surname= document.getElementById('surname').value;
     let email= document.getElementById('email-new-user').value;
-    let password= document.getElementById('password').value;
+    let password= document.getElementById('password-new.user').value;
     let confirmPass= document.getElementById('confirm-pass').value;
     let country= document.getElementById('country').value;
     let city= document.getElementById('city').value;

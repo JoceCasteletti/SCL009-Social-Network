2c3ee5a6c51bd9dd601326a2142faef0317b4ac2
natiacostap <nati_aap@hotmail.com>
2019-06-04 14:50:51 -0400

    borre funcion validate para remplazarla por majo funcion

--- src/assets/js/firebaseAuth.js
+++ src/assets/js/firebaseAuth.js
@@ -1,31 +1,5 @@
 // import { templateNewUser} from'./assets/views/templateNewUser.js';
 
-//CREAR NUEVO USUARIO
-
-
- 
-//  })
-
-
-
-// handleSignUp() => {
-//   let email = document.getElementById('email').value;
-//   let password = document.getElementById('password').value;
-//   if (email.length < 4) {
-//     alert('Please enter an email address.');
-//     console.log(email);
-//     return;
-//   }
-//   if (password.length < 4) {
-//     alert('Please enter a password.');
-//     console.log(password);
-//     return;
-//   }
-
-
-
-
-
 firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
--- src/assets/js/validate.js
+++
@@ -1,4 +0,0 @@
-// export const validateNewUser = (name,surname,email,password){
-//     if(name.len)
-// }
-

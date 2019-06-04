963997dc820a048979f0737c504ac20dca88ded9
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-06-04 17:19:55 -0400

    se realizan validaciones de @ y se modifica formulario de new user

--- src/assets/js/validate.js
+++ src/assets/js/validate.js
@@ -9,13 +9,11 @@ export const validateNewUser = (name, surname, email, password, confirmPass, cou
 	return true;
 }
 
-export const checkAtEmail = (email) => {
-	let checkAt = /\S+@\S+\.\S+/;
-	return checkAt.test(email);
+//Validación de @ en email
+//export const checkAtEmail = (email) => {
+	//let checkAt = /\S+@\S+\.\S+/;
+	//return checkAt.test(email);
   
-}
+//}
 
-/*b)Función que valida que el usuario debe ingresar un @ cuando ingresa un correo*/
-
-	/*expresión regular que simula el patron de búsqueda del correo electrónico*/
 	
--- src/assets/views/templateNewUser.js
+++ src/assets/views/templateNewUser.js
@@ -5,37 +5,19 @@ import { createNewUser } from '../js/authdom.js';
 export const templateNewUser = () =>{
 
     document.getElementById('root').innerHTML =
-` <div
-<h1> Nuevo Usuario Patria </h1>
-<form id="form-new-user">
-<div id="error">
-</div>
-<div >
-<input id="name" type="text" class="info-new" placeholder="Nombre">
-</div>
-<div>
-<input id="surname" type="text" class="info-new"" placeholder="Apellido">
-</div>
-<div >
-<input id="email-new-user" type="text" class="info-new"" placeholder="Email">
-</div>
-<div>
-<input id="password-new.user" type="text" class="info-new"" placeholder="Contraseña 6 dígitos">
-</div>
-<div>
-<input id="confirm-pass" type="text" class="info-new"" placeholder="Confirmar contraseña 6 dígitos">
-</div>
-<div>
-<input id="country" type="text" class="info-new"" placeholder="Pais">
-</div>
-<div>
-<input id="city" type="text" class="info-new"" placeholder="Ciudad">
-</div>
-<div"
-<button id="create-acc" type="button" class="">Crear Cuenta </button>
-</div>
-</form>
-</div>`
+    ` <div
+    <h1> Nuevo Usuario Patria </h1>
+    <form id="form-new-user">
+    <div id="error-create-name"><input id="name" type="text" class="info-new" placeholder="Nombre"></div>
+    <div id="error-create-surname"><input id="surname" type="text" class="info-new"" placeholder="Apellido"></div>
+    <div id="error-create-email"><input id="email-new-user" type="text" class="info-new"" placeholder="Email"></div>
+    <div id="error-create-password"><input id="password-new.user" type="text" class="info-new"" placeholder="Contraseña 6 dígitos"></div>
+    <div id="error-create-confirm"><input id="confirm-pass" type="text" class="info-new"" placeholder="Confirmar contraseña 6 dígitos"></div>
+    <div id="error-create-country"><input id="country" type="text" class="info-new"" placeholder="Pais"></div>
+    <div id="error-create-city"><input id="city" type="text" class="info-new"" placeholder="Ciudad"></div>
+    <button id="create-acc" type="button" class="">Crear Cuenta </button>
+    </form>
+    </div>`
 
 document.getElementById('create-acc').addEventListener('click', () => {
     createNewUser();

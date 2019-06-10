8db5d0c5bd2d71b97cb74516769d3564e2ce9d0e
natiacostap <nati_aap@hotmail.com>
2019-06-10 17:20:54 -0400

    agregamos icono google en boton iniciar sesion con google

--- src/assets/views/templateHomeSingIn.js
+++ src/assets/views/templateHomeSingIn.js
@@ -25,7 +25,8 @@ export const templateHomeSingIn = () => {
                   </div>
                   <div class="form-group">
                      <button type="button" id="btn-go" >Entrar</button>
-                     <button type="button" id="sign-in-gmail" >Iniciar con Google</button>
+                     <button type="button" id="sign-in-gmail" ><p id="google">Iniciar con Google</p>
+                    <img class="icon" src="assets/img/logo-google.png"</img></button>
                   </div>
                </div>               
             </form>
